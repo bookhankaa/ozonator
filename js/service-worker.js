f8724ffbe5b1e4f8e3a7fc1b8b6e798f09d578d2
bookhankaa <user.email>
2024-04-30 21:45:58 +0200

    init

---
+++ js/service-worker.js
@@ -0,0 +1,46 @@
+
+function cleanupPage() {
+  const elements = document.getElementsByClassName("r5f")
+  var elementsArr = Array.prototype.slice.call(elements)
+  elementsArr.sort((a, b) => {
+    const [aDate, bDate] = [getDateDelivery(a), getDateDelivery(b)]
+    return +(aDate > bDate) | -(aDate < bDate) | 0
+  })
+
+  var parent = document.querySelector(".rf6")
+  elementsArr.forEach(element => {
+    parent.append(element)
+  })
+
+  function getDateDelivery(e) {
+    const datesMap = {
+      "января": "January",
+      "февраля": "February",
+      "марта": "March",
+      "апреля": "April",
+      "мая": "May",
+      "июня": "June",
+      "июля": "July",
+      "августа": "August",
+      "сентября": "September",
+      "октября": "October",
+      "ноября": "November",
+      "декабря": "December",
+    }
+    var delivery_string = Array.prototype.slice.call(e.querySelectorAll(".er0")).at(-1).innerHTML.split(":")[1].trim().split(" ").slice(0, 2)
+    if (!parseInt(delivery_string[2])) {
+      delivery_string[2] = (new Date()).getFullYear()
+    }
+    delivery_string[1] = datesMap[delivery_string[1].toLowerCase()] || delivery_string[1]
+    return new Date(delivery_string)
+  }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  if (!tab.url.includes('chrome://')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: cleanupPage
+    })
+  }
+})
