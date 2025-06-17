feb7e2a62c4444c00d46c613ff06d0d275348071
bookhankaa <user.email>
2025-06-17 18:12:47 +0200

    code totally rewrite

--- js/service-worker.js
+++ js/service-worker.js
@@ -18,49 +18,44 @@ function sortCurrentOrders() {
   const date_class_name = '.' + document.evaluate("//p[contains(text(),'дата')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.className
   const elements = document.querySelectorAll('[data-widget="orderList"] > div')
   var elementsArr = Array.prototype.slice.call(elements)
+  var parent = document.querySelector('[data-widget="orderList"]')
 
   elementsArr.sort((a, b) => {
     const [aDate, bDate] = [getDateDelivery(a), getDateDelivery(b)]
-    if (aDate == null) return -1
-    if (bDate == null) return +1
     return +(aDate > bDate) | -(aDate < bDate)
   })
 
-  var parent = document.querySelector('[data-widget="orderList"]')
   elementsArr.forEach(element => {
-    const txtcont = element.textContent
-    if (!txtcont.includes('В пути')) {
-      element.remove()
-    }
-    else {
+    const substrings = [
+        'В пути',
+        'Ожидает получения',
+        'В сборке',
+        'Передаётся в доставку'
+      ]
+    if (substrings.some(sub => element.textContent.includes(sub))) {
       parent.append(element)
     }
-  })
-
-
-  function getDateDelivery(e) {
-    try {
-      var delivery_string = Array.prototype.slice.call(e.querySelectorAll(date_class_name)).at(-1).innerHTML.split(":")[1]
-      console.log(delivery_string)
-      if (!delivery_string) {
-        return null
-      }
-      delivery_string = delivery_string.trim().split(" ").slice(0, 2)
-      if (!parseInt(delivery_string[2])) {
-        delivery_string[2] = (new Date()).getFullYear()
-      }
-      if (delivery_string[1]) {
-        delivery_string[1] = datesMap[delivery_string[1].toLowerCase()] || delivery_string[1]
-        const delivery_date = new Date(delivery_string)
-        return delivery_date == "Invalid Date" ? undefined : delivery_date
-      }
-      else {
-        return undefined
-      }
-    } catch (error) {
-      console.error(error)
-      return undefined
+    else {
+      element.remove()
     }
+  })
+    
+  function getDateDelivery(e) {
+      var dates_arr = []
+      Array.prototype.slice.call(e.querySelectorAll(date_class_name)).forEach(element => {
+        const text = element.innerText.toLocaleLowerCase()
+        if (text.includes("дата") & !text.includes("отмен")) {
+          var delivery_string = text.split(":")[1].trim().split(" ").slice(0, 2)
+          delivery_string[2] = (new Date()).getFullYear()
+          delivery_string[1] = datesMap[delivery_string[1]] || delivery_string[1]
+          dates_arr.push(new Date(delivery_string))
+        }
+      })
+      if (dates_arr.length > 0) {
+        return new Date(Math.min(...dates_arr.map(d => d.getTime())))
+      } else {
+        return new Date()
+      }
   }
 }
 
